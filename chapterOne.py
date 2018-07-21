cd807f560189de5214dff1a40d17e6cab967867c
Falon Darville <falondarville@gmail.com>
2018-07-21 00:00:58 -0700

    chapter one average pace exercise

---
+++ chapterOne.py
@@ -0,0 +1,20 @@
+# How many seconds in 42 min and 42 secs?
+print(42 * 60 + 42)
+# 42 minutes times 60 seconds per minute, plus 42 seconds is 2562
+
+# How many miles are there in 10 kilometers?
+print(10/1.61)
+# There are 1.61 kilometers in a mile so just divide 10 by 1.61 to get the number of miles to get 6.2
+
+# If you run 10 kilometers in 42 min and 42 secs, what is your average pace (time per mile in minutes and seconds)? What is your MPH?
+print(2562/6.2)
+# this gets us the amount of seconds on average that it takes use to run each mile, which is 413. Then we need to take 413 and convert it back into minutes.
+print(413/60)
+# this gives us 6.88. We need to find out how many seconds equate to .88 of a minute. 
+print(.88 * 60)
+# this is 53, rounded up from 52.8
+
+# Our average pace is 6 minutes and 53 seconds per mile. 
+
+
+
